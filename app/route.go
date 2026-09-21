97f48f723cb5c692ca03d5aef316b25753744f2d
vicent <vicent@vicentdev.com>
2026-09-21 17:38:28 +0200

    routes approach refactor

--- app/route.go
+++ app/route.go
@@ -10,32 +10,43 @@ func (s *server) routes() {
 	s.r.Use(loggingMiddleware)
 	s.r.Use(jsonMiddleware)
 
+	// auth handler
+	authR := s.r.PathPrefix("/auth").Subrouter()
+	authR.PathPrefix("/login").HandlerFunc(s.loginHandler()).Methods("POST")
+	authR.PathPrefix("/logout").HandlerFunc(s.logoutHandler()).Methods("POST")
+
+	// default router handler
+	s.r.PathPrefix("/").HandlerFunc(s.defaultRouteHandler())
+}
+
+func (s *server) defaultRouteHandler() func(http.ResponseWriter, *http.Request) {
+
 	cache := cache.NewRedis[*request.Request](s.rdb)
 	client := request.NewClient(&cache)
 
-	// auth handler
-	authR := s.r.PathPrefix("/auth").Subrouter()
-
-	authR.PathPrefix("/login").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		response := make(map[string]any)
-		response[""] = "login"
-
-		s.writeResponse(w, response)
-	}).Methods("POST")
-
-	authR.PathPrefix("/logout").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		response := make(map[string]any)
-		response[""] = "logout"
-
-		s.writeResponse(w, response)
-	}).Methods("POST")
-
-	// default router handler
-	s.r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		response := make(map[string]any)
 
 		client.Request(w, r)
 
 		s.writeResponse(w, response)
-	})
+	}
+}
+
+func (s *server) loginHandler() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		response := make(map[string]any)
+		response[""] = "login"
+
+		s.writeResponse(w, response)
+	}
+}
+
+func (s *server) logoutHandler() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		response := make(map[string]any)
+		response[""] = "logout"
+
+		s.writeResponse(w, response)
+	}
 }
